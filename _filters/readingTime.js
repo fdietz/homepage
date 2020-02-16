efbd992af3ebfc082beb4df152706b6ab604649a
Frederik Dietz <fdietz@gmail.com>
2020-02-16 14:25:52 +0100

    fix: readingTime

--- _filters/readingTime.js
+++ _filters/readingTime.js
@@ -8,15 +8,6 @@ module.exports = function readingTime(htmlContent) {
   const matches = content.match(/[\u0400-\u04FF]+|\S+\s*/g);
   const count = matches !== null ? matches.length : 0;
 
-  let est = "";
-
   const min = Math.ceil(count / speed);
-
-  if (raw === false) {
-    est = min + " min";
-  } else {
-    est = min;
-  }
-
-  return est;
+  return min + " min";
 };
